b59250558c7796983007feabbcf0170e39bab265
Olga Lorenz <ol900ivas@gmail.com>
2023-04-27 10:27:46 +0300

    Update Loader.jsx

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -17,14 +17,3 @@ export const Loader = () => {
     </Div>
   );
 };
-
-<ThreeDots
-  height="80"
-  width="80"
-  radius="9"
-  color="#4fa94d"
-  ariaLabel="three-dots-loading"
-  wrapperStyle={{}}
-  wrapperClassName=""
-  visible={true}
-/>;
